b02065dc00938a01db69adaf8eeed92aef301c1b
Felipe Zanardo <felipebueno6@hotmail.com>
2023-03-30 22:06:21 -0300

    Documentação usando OpenApi - Swagger

--- minha-quina/src/main/java/tech/ada/minhaquina/api/admin/user/UserManagerRestController.java
+++ minha-quina/src/main/java/tech/ada/minhaquina/api/admin/user/UserManagerRestController.java
@@ -1,5 +1,9 @@
 package tech.ada.minhaquina.api.admin.user;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,14 +14,18 @@ import tech.ada.minhaquina.api.usuario.UsuarioResponse;
 
 import java.util.List;
 
+@SecurityRequirement(name = "Admin")
 @RestController
 @RequestMapping("/admin/users")
 @PreAuthorize("hasRole('ADMIN')")
 @RequiredArgsConstructor
+@Tag(name = "Usuários")
 public class UserManagerRestController {
 
     private final UsuarioRepository userJpaRepository;
 
+
+    @Operation(description = "Lista usuários cadastrados")
     @GetMapping
     public List<UsuarioResponse> listar() {
         return this.userJpaRepository.findAll().stream()
--- minha-quina/src/main/java/tech/ada/minhaquina/api/auth/AuthenticationRestController.java
+++ minha-quina/src/main/java/tech/ada/minhaquina/api/auth/AuthenticationRestController.java
@@ -1,5 +1,8 @@
 package tech.ada.minhaquina.api.auth;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.java.Log;
 import org.springframework.http.HttpStatus;
@@ -11,6 +14,7 @@ import tech.ada.minhaquina.api.system.JwtService;
 import tech.ada.minhaquina.api.usuario.UsuarioModel;
 import tech.ada.minhaquina.api.usuario.UsuarioRepository;
 
+@Tag(name = "Autenticação")
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
@@ -21,6 +25,7 @@ public class AuthenticationRestController {
     private final AuthenticationManager authenticationManager;
     private final JwtService jwtService;
 
+    @Operation(description = "Login de usuário/admin")
     @PostMapping("/login")
     @ResponseStatus(HttpStatus.OK)
     public AuthenticationResponse login(@RequestBody AuthenticationRequest request) {
@@ -30,7 +35,9 @@ public class AuthenticationRestController {
         String token = jwtService.createToken(user);
         return new AuthenticationResponse(token);
     }
-
+    @SecurityRequirement(name = "Admin")
+    @SecurityRequirement(name = "User")
+    @Operation(description = "Logout de usuário/admin")
     @GetMapping("/logout")
     public ResponseEntity<String> logout(@RequestHeader("Authorization") String authorization) {
         String token = authorization.split(" ")[1];
